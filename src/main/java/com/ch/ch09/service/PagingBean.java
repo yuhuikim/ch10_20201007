c8fbb76f05cc9d8cdadf4c03ab468cca97018b5e
pang9 <yuheecong@gmail.com>
2020-10-06 17:34:56 +0900

    third

--- src/main/java/com/ch/ch09/service/PagingBean.java
+++ src/main/java/com/ch/ch09/service/PagingBean.java
@@ -1,5 +1,4 @@
 package com.ch.ch09.service;
-
 import lombok.Data;
 
 @Data
@@ -20,13 +19,13 @@ public class PagingBean {
 		this.total = total;
 
 		// ﻿총 페이지 = Math.ceil(데이터 총 개수 / 페이지 당 개수)
-		totalPage = (int)Math.ceil((double)total/rowPerPage);
-		// 시작 페이지 = (현재 페이지 -1)/10 * 10 +1
-		startPage = currentPage - (currentPage -1) % pagePerBlock;
+		totalPage = (int) Math.ceil((double) total / rowPerPage);
+		// 시작 페이지 = 현재 페이지-(현재 페이지 -1)%10
+		startPage = currentPage - (currentPage - 1) % pagePerBlock;
 		// ﻿끝 페이지 = 시작 페이지 + 블록 당 페이지 수 - 1
-		endPage = startPage + pagePerBlock -1; 
+		endPage = startPage + pagePerBlock - 1;
 		// 마지막 블럭의 page 조정
-		if (endPage > totalPage) endPage = totalPage;
+		if (endPage > totalPage)
+			endPage = totalPage;
 	}
-
 }
